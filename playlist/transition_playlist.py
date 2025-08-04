b4c0431152c81b69a88ce304386c5e63cd0eb6e0
Oscar Chung <oscar.chung.pa@gmail.com>
2025-08-04 02:01:59 -0700

    no ratio

--- playlist/transition_playlist.py
+++ playlist/transition_playlist.py
@@ -234,7 +234,7 @@ def create_transition(songs_dir, transition_type="crossfade"):
         final_transition = part1 + part1_5 + part2 + part2_5 + part3
         output_file = songs_dir + "/dj_transition.mp3"
 
-        start_b = int((vocals_transition_in + tease_duration_ms + crossfade_duration) * ratio1)
+        start_b = vocals_transition_in + tease_duration_ms + crossfade_duration
     
     else:
         raise ValueError(f"Unsupported transition type: {transition_type}")
